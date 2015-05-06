7a71c4bd04e5ff01a7f9b174fc7b4f6bdbb028f0
klemens.morgenstern <klemens.morgenstern@gmx.net>
2015-05-06 19:24:54 +0200

    copied the proposal stuff

---
+++ include/boost/visitor/detail/functor_visitor_deduction.hpp
@@ -0,0 +1,160 @@
+//  Boost.Varaint
+//  Lambda Visitor utilities defined here
+//
+//  See http://www.boost.org for most recent version, including documentation.
+//
+//  Copyright Klemens Morgenstern, 2015.
+//
+//  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef BOOST_VARIANT_DETAIL_FUNCTOR_VISITOR_DEDUCTION_HPP
+#define BOOST_VARIANT_DETAIL_FUNCTOR_VISITOR_DEDUCTION_HPP
+
+#include <boost/type_traits/is_same.hpp>
+#include <boost/core/enable_if.hpp>
+#include <boost/type_traits/remove_reference.hpp>
+#include <boost/type_traits/remove_cv.hpp>
+#include <boost/type_traits/integral_constant.hpp>
+
+namespace boost {
+namespace detail { namespace variant {
+
+//cause i don't wanna include utility; doesn't really make sense, since i already use c++14
+template< class T >
+constexpr T&& forward( typename remove_reference<T>::type& t ) noexcept
+{
+	return static_cast<T&&>(t);
+}
+template< class T >
+constexpr T&& forward( typename remove_reference<T>::type&& t )noexcept
+{
+	return static_cast<T&&>(t);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// functor_parameter_deduction deduct the return type and the parameters of the input.
+//
+//
+template<typename lambda>
+struct functor_parameter_deduction
+{
+};
+//the ... comes from possible mutli visitors.
+template<typename lambda, typename Ret, typename ...Args>
+struct functor_parameter_deduction<Ret(lambda::*)(Args...) const>
+{
+//	typedef Arg0 type0;
+	static constexpr size_t arg_cout = sizeof...(Args);
+	typedef Ret return_type;
+};
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// used for the actual deduction
+//
+template<typename Lambda>
+struct functor_deduct_parameter
+{
+	//using types		  = typename functor_parameter_deduction<decltype(&Lambda::operator())>::types;
+	using return_type = typename functor_parameter_deduction<decltype(&Lambda::operator())>::return_type;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  declaration of the actual visitor, i.e. this one is partial and gets inherited
+//
+template<typename ... Args>
+class functor_visitor_helper
+{
+	void operator()();
+};
+
+template<typename Lambda>
+class functor_visitor_helper<Lambda> : public Lambda
+{
+	using father = Lambda;
+public:
+	using return_type = typename functor_deduct_parameter<Lambda>::return_type;
+	using father::operator();
+
+	template<typename L>
+	functor_visitor_helper(L &&lambda) : father(forward<L>(lambda)) {};
+};
+
+template<typename Lambda, typename ... Args>
+class functor_visitor_helper<Lambda, Args...> : public functor_visitor_helper<Args...>, Lambda
+{
+	using father = Lambda;
+public:
+	using functor_visitor_helper<Args...>::operator();
+	using return_type = typename functor_deduct_parameter<Lambda>::return_type;
+	using father::operator();
+
+	static_assert(boost::is_same<return_type, typename functor_visitor_helper<Args...>::return_type>::value, "return types must have the same type");
+
+
+	template<typename L, typename ... l_Args>
+	functor_visitor_helper(L && lambda, l_Args && ...ls) :
+			functor_visitor_helper<Args...>(forward<l_Args>(ls)...), father(forward<L>(lambda)) {};
+};
+
+template<typename ...Args>
+struct functor_visitor : public functor_visitor_helper<Args...>, public boost::static_visitor<typename functor_visitor_helper<Args...>::return_type>
+{
+	using return_type = typename functor_visitor_helper<Args...>::return_type;
+	using functor_visitor_helper<Args...>::operator();
+	functor_visitor(Args&&...args) : functor_visitor_helper<Args...>(forward<Args>(args)...) {};
+};
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  explicit lambda visitor
+//
+template<typename ... Args>
+class functor_visitor_helper_exp
+{
+	void operator()();
+};
+
+
+
+template<typename Lambda>
+class functor_visitor_helper_exp<Lambda> : public Lambda
+{
+	using father = Lambda;
+public:
+	using father::operator();
+	template<typename L>
+	functor_visitor_helper_exp(L &&lambda) : father(forward<L>(lambda)) {};
+};
+
+template<typename Lambda, typename ... Args>
+class functor_visitor_helper_exp<Lambda, Args...> : public functor_visitor_helper_exp<Args...>, public Lambda
+{
+	using father = Lambda;
+public:
+	using functor_visitor_helper_exp<Args...>::operator();
+	using father::operator();
+
+	template<typename L, typename ... l_Args>
+	functor_visitor_helper_exp(L && lambda, l_Args && ...ls) :
+		functor_visitor_helper_exp<l_Args...>(forward<l_Args>(ls)...), father(forward<L>(lambda)) {};
+};
+
+template<typename ReturnType, typename ...Args>
+struct functor_visitor_exp : public functor_visitor_helper_exp<Args...>, public boost::static_visitor<ReturnType>
+{
+	using return_type = ReturnType;
+	using functor_visitor_helper_exp<Args...>::operator();
+	functor_visitor_exp(Args&&...args) : functor_visitor_helper_exp<Args...>(forward<Args>(args)...) {};
+};
+
+}}}
+
+
+
+
+#endif /* BOOST_VARIANT_DETAIL_functor_VISITOR_DEDUCTION_HPP_ */
---
+++ include/boost/visitor/static_functor_visitor.hpp
@@ -0,0 +1,52 @@
+//  Boost.Varaint
+//  Multivisitors defined here
+//
+//  See http://www.boost.org for most recent version, including documentation.
+//
+//  Copyright Klemens Morgenstern, 2015.
+//
+//  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef BOOST_VISITOR_STATIC_FUNCTOR_VISITOR_HPP
+#define BOOST_VISITOR_STATIC_FUNCTOR_VISITOR_HPP
+
+#include "boost/visitor/static_visitor.hpp"
+#include "boost/visitor/detail/functor_visitor_deduction.hpp"
+
+namespace boost {
+
+template<typename ...Lambdas>
+detail::variant::functor_visitor<Lambdas...> make_lambda_visitor(Lambdas && ...args) {return detail::variant::functor_visitor<Lambdas...>(detail::variant::forward<Lambdas>(args)...);}
+
+
+template<typename Variant, typename ...Lambdas>
+auto apply_lambdas(Variant & var, Lambdas ... lambdas) -> typename detail::variant::functor_visitor<Lambdas...>::return_type
+{
+	using Ret = typename detail::variant::functor_visitor<Lambdas...>::return_type;
+	auto vis = make_lambda_visitor(detail::variant::forward<Lambdas>(lambdas)...);
+	return Ret(apply_visitor(vis, var));//type
+};
+
+///explicit version
+
+template<typename ReturnType, typename ...Lambdas>
+detail::variant::functor_visitor_exp<ReturnType, Lambdas...> make_lambda_visitor(Lambdas && ...args) {return detail::variant::functor_visitor_exp<ReturnType, Lambdas...>(detail::variant::forward<Lambdas>(args)...);}
+
+
+template<typename ReturnType, typename Variant, typename ...Lambdas>
+auto apply_lambdas(Variant & var, Lambdas ... lambdas) -> ReturnType
+{
+//	using Ret = typename detail::variant::lambda_visitor<Lambdas...>::return_type;
+	auto vis = make_lambda_visitor<ReturnType>(detail::variant::forward<Lambdas>(lambdas)...);
+	return ReturnType(apply_visitor(vis, var));//type
+};
+
+
+
+}
+
+
+
+#endif /* LAMBDA_VISITOR_H_ */
