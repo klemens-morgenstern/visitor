7b979d3f9dbcbea224f9f00e3bf468f39d122f64
klemens.morgenstern <klemens.morgenstern@gmx.net>
2015-05-17 17:02:19 +0200

    added a little documentation

--- include/boost/visitor/deduct_variant.hpp
+++ include/boost/visitor/deduct_variant.hpp
@@ -5,11 +5,11 @@
  *      Author: Klemens
  */
 
-#ifndef BOOST_VISITOR_ADAPT_VARIANT_HPP_
-#define BOOST_VISITOR_ADAPT_VARIANT_HPP_
+#ifndef BOOST_VISITOR_DEDUCT_VARIANT_HPP_
+#define BOOST_VISITOR_DEDUCT_VARIANT_HPP_
 
 #include <boost/visitor/detail/poly_from_variant.hpp>
 
 
 
-#endif /* BOOST_VISITOR_ADAPT_VARIANT_HPP_ */
+#endif /* BOOST_VISITOR_DEDUCT_VARIANT_HPP_ */
