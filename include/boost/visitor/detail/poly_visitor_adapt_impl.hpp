d41e969a3db3dbe4ea87016daf2e76eea08aef22
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2015-10-03 19:50:17 +0200

    Fixed the problem

--- include/boost/visitor/detail/poly_visitor_adapt_impl.hpp
+++ include/boost/visitor/detail/poly_visitor_adapt_impl.hpp
@@ -22,7 +22,7 @@ template<typename Visitor, typename Abstract>
 struct end_abstract : Abstract
 {
 	Visitor &vis;
-	end_abstract(Visitor vis) : vis(vis) {};
+	end_abstract(Visitor &vis) : vis(vis) {};
 	end_abstract(const end_abstract&) = default;
 
 };
